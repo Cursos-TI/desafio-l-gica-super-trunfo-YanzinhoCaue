afccfed1d8f6aa2ef341a1b3291778abd3b2b79c
Yan Cauê <127339610+YanzinhoCaue@users.noreply.github.com>
2025-01-22 20:22:50 +0000

    Cadastrando as cartas

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,6 +6,75 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+
+    //Card 1
+    int zipcodeOne, populationOne, ageOne, touristAttractionsOne, cityOneWins;
+    char cityNameOne[50], cardCodeOne[3];
+    float areaOne, gdpOne, populationDensityOne, gdpPerCapitaOne, calculateMediaOne;
+
+    int zipcodeTwo, populationTwo, ageTwo, touristAttractionsTwo, cityTwoWins;
+    char cityNameTwo[50], cardCodeTwo[3];
+    float areaTwo, gdpTwo, populationDensityTwo, gdpPerCapitaTwo, calculateMediaTwo;
+
+    //Introdução ao jogo
+    printf("================================\n");
+    printf("=======JOGO SUPER TRUNFO========\n");
+    printf("================================\n");
+
+    //Cadastrando a Primeira Carta
+    pritf("==CADASTRE A PRIMEIRA CARTA==\n");
+
+    printf("Digite o código da primeira carta:\n");
+    scanf("%d", &cardCodeOne);
+
+    printf("Digite o CEP da primeira carta:\n");
+    scanf("%d", &zipcodeOne);
+
+    printf("Digite o nome da primeira carta:\n");
+    scanf("%s", &cityNameOne);
+
+    printf("Digite a idade da primeira carta:\n");
+    scanf("%d", %ageOne);
+
+    printf("Digite a area geográfica da primeira carta:\n");
+    scanf("%f", &areaOne);
+
+    printf("Digite o numero de Pontos Turisticos da primeira carta:\n");
+    scanf("%d", &touristAttractionsOne);
+
+    printf("Digite o PIB da primeira carta:\n");
+    scanf("%f", &gdpOne);
+
+    printf("=======CARTA REGISTRADA COM SUCESSO========\n");
+
+    //Cadastrando a Segunda Carta
+    pritf("==CADASTRE A SEGUNDA CARTA==\n");
+
+    printf("Digite o código da segunda carta:\n");
+    scanf("%d", &cardCodeTwo);
+
+    printf("Digite o CEP da segunda carta:\n");
+    scanf("%d", &zipcodeTwo);
+
+    printf("Digite o nome da segunda carta:\n");
+    scanf("%s", &cityNameTwo);
+
+    printf("Digite a idade da segunda carta:\n");
+    scanf("%d", %ageTwo);
+
+    printf("Digite a area geográfica da segunda carta:\n");
+    scanf("%f", &areaTwo);
+
+    printf("Digite o numero de Pontos Turisticos da segunda carta:\n");
+    scanf("%d", &touristAttractionsTwo);
+
+    printf("Digite o PIB da segunda carta:\n");
+    scanf("%f", &gdpTwo);
+
+    printf("=======CARTA REGISTRADA COM SUCESSO========\n");
+
+    
+
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
