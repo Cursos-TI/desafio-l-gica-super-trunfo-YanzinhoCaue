0ea60ad1fc64e83f0c4d7c428606fc80f13b9b2c
Yan Cauê <127339610+YanzinhoCaue@users.noreply.github.com>
2025-01-23 16:50:53 +0000

    Desafio novato completo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -119,39 +119,11 @@ int main() {
     printf("=======RESULTADO========\n");
     if (mediaOneCity > mediaTwoCity) {
         printf("A cidade da primeira carta é a vencedora!!\n");
+    } else if (mediaOneCity = mediaTwoCity) {
+        printf("As cidades empataram!!\n");
+    } else {
+        printf("A cidade da segunda carta é a vencedora!!\n");
     }
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
-
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
-
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
-
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
-
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
-
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
 
     return 0;
 }
